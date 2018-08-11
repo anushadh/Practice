1906ab7e3d13512831c489d38ac7a09adb1ce1a3
Anusha <anushadhulipudi.ad@gmail.com>
2018-08-11 16:44:34 +0530

    Algos practice
    count digits till single number

--- LeetCode/src/problems/algorithms/AddDigits.java
+++ LeetCode/src/problems/algorithms/AddDigits.java
@@ -1,10 +1,33 @@
 package problems.algorithms;
 
+/*Problem 258:
+	
+	Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+
+*/
 public class AddDigits {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AddDigits ad = new AddDigits();
+		int num = 999;
+		System.out.println(ad.addDigits(num));;
 	}
 
+	public int addDigits(int num) {
+		if(num < 10) {
+			return num;
+		}
+		int sum = 0;
+		int rem = 0;
+		while(num > 0) {
+			rem = num%10;
+			num = num/10;
+			sum = sum + rem;
+			if(num == 0 && sum > 9) {
+				num = sum;
+				sum = 0;
+			}
+		}
+		return sum;
+	}
 }
