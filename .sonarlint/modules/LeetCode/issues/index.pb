ba8fa8f9e99fbb03e78ddb2e3ce711efcd6e36bb
Anusha <anushadhulipudi.ad@gmail.com>
2018-08-27 10:20:31 +0530

    Algos practice
    Unique morse code transformation of strings

---
+++ .sonarlint/modules/LeetCode/issues/index.pb
@@ -0,0 +1,5 @@
+
+n
+>src/problems/algorithms/RemoveDuplicatesfromSortedArrayII.java,e\d\edbdfc5aeb57f64b6fbbe875bc3dba3433299a1e
+S
+#src/problems/contest/TestClass.java,7\8\787d9c98bd392d34ac5636d2149e65586f66948a
--- LeetCode/src/problems/algorithms/MoveZeroes.java
+++ LeetCode/src/problems/algorithms/MoveZeroes.java
@@ -24,10 +24,9 @@ public class MoveZeroes {
         if(nums.length > 0) {
        
         	int index = 0;
-        	for(int i = 0; i < nums.length && index < nums.length; i++) {
+        	for(int i = 0; i < nums.length; i++) {
         		if(nums[i] != 0) {
-        			nums[index] = nums[i];
-        			index++;
+        			nums[index++] = nums[i];
         		}
         	}
         	for(int i = index; i < nums.length; i++) {
